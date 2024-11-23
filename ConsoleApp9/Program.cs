c378464fff10ec4f23cd16f072d65c716036ddd1
Someo <Someo@SOMEONE_800>
2024-11-23 15:24:14 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму вклада:");
+
+            double depositAmount;
+            while (!double.TryParse(Console.ReadLine(), out depositAmount) || depositAmount < 0)
+            {
+                Console.WriteLine("Неверный ввод. Пожалуйста, введите положительное число.");
+                Console.WriteLine("Введите сумму вклада:");
+            }
+
+            double interestRate;
+            if (depositAmount < 100)
+            {
+                interestRate = 0.05; // 5%
+            }
+            else if (depositAmount >= 100 && depositAmount <= 200)
+            {
+                interestRate = 0.07; // 7%
+            }
+            else
+            {
+                interestRate = 0.10; // 10%
+            }
+
+            double interestAmount = depositAmount * interestRate;
+            double totalAmount = depositAmount + interestAmount;
+
+            Console.WriteLine($"Сумма вклада: {depositAmount:C}");
+            Console.WriteLine($"Начисленные проценты: {interestAmount:C}");
+            Console.WriteLine($"Итоговая сумма: {totalAmount:C}");
+            Console.ReadLine();
+        }
+    }
+}
+
